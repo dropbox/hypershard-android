b67f41837ecdef4e2f9e0172e5a0de6f7473462f
David Chang <changd@gmail.com>
2019-10-14 23:05:58 -0700

    Update example python script

--- example/run_hypershard.py
+++ example/run_hypershard.py
@@ -23,10 +23,9 @@ if __name__ == "__main__":
 
     if not os.path.isfile(file_name):    
         url_opener = urllib.URLopener()
-        url_opener.retrieve("https://search.maven.org/remotecontent?filepath=com/dropbox/mobile/hypershard/hypershard/1.0.0/hypershard-1.0.0.jar", file_name)
+        url_opener.retrieve("https://oss.sonatype.org/service/local/repositories/snapshots/content/com/dropbox/mobile/hypershard/hypershard/1.0.1-SNAPSHOT/hypershard-1.0.1-20191015.055746-1.jar", file_name)
 
-    # TODO Update after 1.0.1 is uploaded
-    hypershard_command = "java -jar hypershard.jar UiTest src/test/resources"
+    hypershard_command = "java -jar hypershard.jar --annotation-name UiTest src/test/resources"
     print "Input: \n", hypershard_command
     output = subprocess.check_output(hypershard_command.split())
     print "Output: \n" , output
